0cbd87faa0fef5a4e8a76ca08062e6d387d91413
Givanaldo Rocha <givanaldo@gmail.com>
2024-02-25 20:12:38 -0300

    modify hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,7 @@
 let nome = "Fulano Dital";
 console.log("Olá " + nome);
-console.log(typeof(nome))
+console.log(typeof(nome));
+
+let idade = 20;
+console.log("Idade: " + idade);
+console.log(typeof(idade));
