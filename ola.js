91e9379054622ff7fbd28133263c6ec81034a165
Givanaldo <givanaldo@gmail.com>
2024-02-26 08:27:01 -0300

    atualização

--- ola.js
+++ ola.js
@@ -3,4 +3,4 @@ alert("Escrevendo no navegador");
 let dia = prompt("Que dia é hoje?");
 console.log("Hoje é " + dia);
 alert("Hoje é " + dia);
-document.querySelector('#dia').innerHTML = 'Hoje é ' + dia;
+document.getElementById('dia').innerHTML = 'Hoje é ' + dia;
