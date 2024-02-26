3dbd45e8c7a724e0c1c4874e6fc32a4d56881850
Givanaldo <givanaldo@gmail.com>
2024-02-26 08:10:50 -0300

    arquivos da primeira aula

---
+++ ola.js
@@ -0,0 +1,6 @@
+console.log('Oi, tudo bem!'); 
+alert("Escrevendo no navegador");
+let dia = prompt("Que dia é hoje?");
+console.log("Hoje é " + dia);
+alert("Hoje é " + dia);
+document.querySelector('#dia').innerHTML = 'Hoje é ' + dia;
