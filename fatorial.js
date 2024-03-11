a757d35b831ffd5eaa9f3ee02ff7b4471c2a43dc
Givanaldo Rocha <givanaldo@gmail.com>
2024-03-11 14:28:46 -0300

    exemplos

--- fatorial.js
+++ fatorial.js
@@ -6,3 +6,11 @@ function fatorial(n) {
 }
 
 console.log(fatorial(5));
+
+function fatorial() {
+    let n = document.getElementById("numero").value;
+    let fat = 1;
+    for (let i=2; i<=n; i++)
+        fat = fat * i;
+    document.getElementById("resultado").innerHTML = `Fatorial(${n}) = ${fat}`;
+}
