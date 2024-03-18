c9e20ab5f5fd3bc189b7eab709a4756b2ff82a54
Givanaldo <givanaldo@gmail.com>
2024-03-18 07:27:12 -0300

    exercicios

--- lista_1/q1.js
+++ lista_1/q1.js
@@ -21,7 +21,7 @@ function calcularIdade(dataNascimento) {
 //console.log(calcularIdade("08/06/1980"));
 
 function calcularIdade1() {
-    let dataNascimento = document.querySelector("#data").value;
+    let dataNascimento = document.querySelector("#data1").value;
     let partesData = dataNascimento.split("/");
     let diaNascimento = parseInt(partesData[0]);
     let mesNascimento = parseInt(partesData[1]);
@@ -41,17 +41,16 @@ function calcularIdade1() {
     document.querySelector("#idade").innerHTML = `Idade: ${idade}`;
 }
 
-function calcularIdade1() {
-    let dataNascimento = document.querySelector("#data").value;
-    let partesData = dataNascimento.split("/");
-    let diaNascimento = parseInt(partesData[0]);
-    let mesNascimento = parseInt(partesData[1]);
-    let anoNascimento = parseInt(partesData[2]);
+function calcularIdade2() {
+    let dataNascimento = new Date(document.querySelector("#data2").value);
+    let diaNascimento = dataNascimento.getDate();
+    let mesNascimento = dataNascimento.getMonth() + 1;
+    let anoNascimento = dataNascimento.getFullYear();
 
     let hoje = new Date();
-    let anoAtual = hoje.getFullYear();
-    let mesAtual = hoje.getMonth() + 1;
     let diaAtual = hoje.getDate();
+    let mesAtual = hoje.getMonth() + 1;
+    let anoAtual = hoje.getFullYear();
 
     let idade = anoAtual - anoNascimento;
 
