97d5a1c3792ab3e1c96f03f8247a09e3c20a91f5
Givanaldo <givanaldo@gmail.com>
2024-04-05 08:30:30 -0300

    vetores

---
+++ vetor1.js
@@ -0,0 +1,8 @@
+let frutas = ["Maçã", "Banana", "Laranja"];
+console.log(frutas);
+console.log(frutas.toString());
+console.log(frutas.join(", "));
+let numeros = [2, 5, 6, 7, 8, 0, 10, 12];
+console.log(numeros);
+let compras = ["Maçã", 2, "Banana", 4, "Laranja", 10];
+console.log(compras);
