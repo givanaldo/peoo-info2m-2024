916d216794db466f19b23b9e38b3006437f1ece9
Givanaldo <givanaldo@gmail.com>
2024-07-15 08:33:44 -0300

    vetores

--- vetor2.js
+++ vetor2.js
@@ -2,16 +2,18 @@ const numeros = [2, 3, 7, 8, 10];
 console.log(numeros.toString());
 
 const quadrados = numeros.map(function(num) {
-    return num ** 2;
-});
+    return num ** 2; });
 console.log(quadrados.toString());
 
 let resultado = numeros.filter(function(num) {
-    return num > 5;
-})
+    return num > 5; })
 console.log(resultado.toString());
 
-resultado = numeros.reduce(function(total, num) {
-    return total * num;
-}, 1);
+resultado = numeros.reduce(function(total, num) { 
+    return total + num; }, 0);
+console.log(resultado);
+
+const frutas = ["tamarindo", "cajá", "tomate"]
+resultado = frutas.filter(function(fruta){
+    return fruta.length >= 6});
 console.log(resultado.toString());
