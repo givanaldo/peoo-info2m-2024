d7d7bbf292b025b259f89bd52008d71831190163
Givanaldo <givanaldo@gmail.com>
2024-08-22 08:32:48 -0300

    exercicios

--- exerc_json/placeholder/q2/q2.js
+++ exerc_json/placeholder/q2/q2.js
@@ -1,10 +1,18 @@
-let post_id = 2;
-let url = `https://jsonplaceholder.typicode.com/comments?postId=${post_id}`;
+function listar() {
+    let post_id = document.getElementById('post_id').value;
+    let url = `https://jsonplaceholder.typicode.com/comments?postId=${post_id}`;
 
-fetch(url)
-  .then((response) => response.json())
-  .then((json) => list_comments(json));
-
+    fetch(url)
+    .then((response) => response.json())
+    .then((json) => list_comments(json));
+}
 function list_comments(json) {
-
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = '';
+    for (let comment of json) {
+        resultado.innerHTML += `<hr>#ID: ${comment.id}<br>
+            Nome: ${comment.name} <br>
+            E-Mail: ${comment.email} <br>
+            Texto: ${comment.body}`;
+    }
 }
