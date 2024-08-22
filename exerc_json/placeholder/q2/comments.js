a78f8d1d15ff5760c043b7096fa57dcf36676422
Givanaldo <givanaldo@gmail.com>
2024-08-21 22:53:55 -0300

    exercicios

--- exerc_json/placeholder/q2/comments.js
+++ exerc_json/placeholder/q2/comments.js
@@ -1,19 +1,34 @@
-function show_posts() {
-    let post_id = 1;
-    let url_comments = `https://jsonplaceholder.typicode.com/comments?postId=${post_id}`;
+const url_posts = 'https://jsonplaceholder.typicode.com/posts';
+fetch(url_posts)
+  .then(response => response.json())
+  .then(posts => list_posts(posts));
 
-    fetch(url_comments)
-      .then(response => response.json())
-      .then(json_data => {
-        console.log(`Post ID: ${post_id}\n=======`);
-        json_data.forEach(comment => {
-            console.log(`#${comment.id}`);
-            console.log(`E-mail: ${comment.email}`);
-            console.log(`Título: ${comment.name}`);
-            console.log(`Texto: ${comment.body}`);
-            console.log('');
-        });
-      });
+function list_posts(posts) {
+  let select = document.getElementById('posts');
+  for (let post of posts) {
+    let option = new Option(post.title, post.id);
+    select.options[select.options.length] = option;
+  }
 }
 
-show_posts();
+function show_comments() {
+  let post_id = document.getElementById('posts').value;
+  let url_comments = `https://jsonplaceholder.typicode.com/comments?postId=${post_id}`;
+  
+  console.log(post_id);
+
+  fetch(url_comments)
+    .then(response => response.json())
+    .then(comments => {
+        let result = document.getElementById('result');
+        result.innerHTML ='';
+        comments.forEach(comment => {
+          let div = document.createElement("div");
+          div.innerHTML = `<hr>#${comment.id} <br>
+            E-mail: ${comment.email} <br>
+            Título: ${comment.name} <br>
+            Texto: ${comment.body} <br><hr>`; 
+          result.appendChild(div);
+        });
+    });
+}
