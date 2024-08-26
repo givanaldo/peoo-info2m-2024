4c90b2939c8c536c0355007887c821769cf33fa5
Givanaldo <givanaldo@gmail.com>
2024-08-26 08:31:54 -0300

    exercicios json

--- exerc_json/placeholder/q5/q5.js
+++ exerc_json/placeholder/q5/q5.js
@@ -1,3 +1,16 @@
+const url_users = 'https://jsonplaceholder.typicode.com/users';
+fetch(url_users)
+  .then(response => response.json())
+  .then(users => list_users(users));
+
+function list_users(users) {
+  let select = document.getElementById('user_id');
+  for (let user of users) {
+    let option = new Option(user.name, user.id);
+    select.options[select.options.length] = option;
+  }
+}
+
 function listar() {
     const user = document.getElementById('user_id').value;
     const url = `https://jsonplaceholder.typicode.com/users/${user}/todos`;
