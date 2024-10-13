f202c06e8020c26d5110190fc9b5a9310b5109a1
Givanaldo <givanaldo@gmail.com>
2024-10-13 19:50:11 -0300

    exercícios

--- exerc_json/timezone/timezones.js
+++ exerc_json/timezone/timezones.js
@@ -1,6 +1,6 @@
 const url = 'http://worldtimeapi.org/api/timezone';
 const timezonesSelect = document.getElementById('timezones');
-const selectedTimezonesContainer = document.getElementById('selected-timezones');
+const resultado = document.getElementById('resultado');
 
 fetch(url)
     .then(response => response.json())
@@ -11,38 +11,30 @@ fetch(url)
             option.textContent = timezone;
             timezonesSelect.appendChild(option);
         });
-    })
-    .catch(error => {
-        console.error('Erro:', error);
     });
 
-    timezonesSelect.addEventListener('change', () => {
-        const selectedOptions = Array.from(timezonesSelect.selectedOptions);
-        selectedOptions.forEach(option => {
-            exibir_horarios(option.value);
+timezonesSelect.addEventListener('change', () => {
+    const selectedOptions = Array.from(timezonesSelect.selectedOptions);
+    selectedOptions.forEach(option => exibir_horarios(option.value));
+});
+
+function exibir_horarios(timezone) {
+    fetch(`http://worldtimeapi.org/api/timezone/${timezone}`)
+        .then(response => response.json())
+        .then(data => {
+            const dateTimeString = data.datetime;
+            const [date, time] = dateTimeString.split('T');
+            const [ano, mes, dia] = date.split('-');
+            const formattedDate = `${dia}/${mes}/${ano}`;
+            const formattedTime = time.split('.')[0];
+
+            const timezoneDiv = document.createElement('div');
+            timezoneDiv.classList.add('timezone');
+            timezoneDiv.id = timezone;
+            timezoneDiv.innerHTML = `<strong>${timezone}:</strong> ${formattedDate} - ${formattedTime} (${data.datetime})`;
+
+            if (!document.getElementById(timezone)) {
+                resultado.appendChild(timezoneDiv);
+            }
         });
-    });
-
-    function exibir_horarios(timezone) {
-        fetch(`http://worldtimeapi.org/api/timezone/${timezone}`)
-            .then(response => response.json())
-            .then(data => {
-                const dateTimeString = data.datetime;
-                const [date, time] = dateTimeString.split('T');
-                const [ano, mes, dia] = date.split('-');
-                const formattedDate = `${dia}/${mes}/${ano}`;
-                const formattedTime = time.split('.')[0];
-
-                const timezoneDiv = document.createElement('div');
-                timezoneDiv.classList.add('timezone');
-                timezoneDiv.id = timezone;
-                timezoneDiv.innerHTML = `<strong>${timezone}:</strong> ${formattedDate} - ${formattedTime} (${data.datetime})`;
-
-                if (!document.getElementById(timezone)) {
-                    selectedTimezonesContainer.appendChild(timezoneDiv);
-                }
-            })
-            .catch(error => {
-                console.error('Erro:', error);
-            });
     }
