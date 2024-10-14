4550c038074fd03d9e85749493e1619869bc93ea
Givanaldo <givanaldo@gmail.com>
2024-10-14 08:28:48 -0300

    timezone json api

---
+++ timezones/timezone.js
@@ -0,0 +1,26 @@
+const url = "https://worldtimeapi.org/api/timezone";
+const select = document.getElementById('timezones');
+const resultado = document.getElementById('resultado');
+
+fetch(url)
+.then(response => response.json())
+.then(timezones => {
+    timezones.forEach(timezone => {
+        const option = document.createElement('option');
+        option.value = timezone;
+        option.textContent = timezone;
+        select.appendChild(option);
+    });
+});
+
+function exibir_fuso() {
+    const timezone = document.getElementById('timezones').value;
+    const urltimezone = `https://worldtimeapi.org/api/timezone/${timezone}`;
+    
+    fetch(urltimezone)
+        .then(response => response.json())
+        .then(fuso => {
+            const datetime = fuso.datetime;
+            resultado.innerHTML = `<b>${timezone}</b>: ${datetime}<br>`;
+        });
+}
