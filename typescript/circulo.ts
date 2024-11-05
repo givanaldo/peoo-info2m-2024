7b63a23781b6d59307aba6f9f293045473131127
Givanaldo <givanaldo@gmail.com>
2024-11-05 10:56:40 -0300

    circulo

--- typescript/circulo.ts
+++ typescript/circulo.ts
@@ -7,6 +7,16 @@ class Circulo {
         this.x = x;
         this.y = y;
     }
+    
+    mover(x: number, y: number) : void {
+        this.x += x; // this.x = this.x + x;
+        this.y += y; // this.y = this.y + y;
+    }
+
+    aumentar(r: number) : void {
+        this.raio += r;
+    }
+
     exibir() : string {
         return `Raio: ${this.raio}, X: ${this.x}, Y: ${this.y}`;
     }
@@ -15,4 +25,5 @@ class Circulo {
 let circulo1 = new Circulo(2, 0, 0);
 let circulo2 = new Circulo(3, 5, 2);
 console.log(circulo1.exibir());
+// circulo2.raio = 10;
 console.log(circulo2.exibir());
