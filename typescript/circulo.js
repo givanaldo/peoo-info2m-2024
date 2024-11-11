642f462a722a1ec3ab5f4517b83606cae5a3dd50
Givanaldo <givanaldo@gmail.com>
2024-11-11 07:18:42 -0300

    exemplos oo

--- typescript/circulo.js
+++ typescript/circulo.js
@@ -1,48 +1,39 @@
-class Circulo {
-    private raio: number;
-    private x: number;
-    private y: number;
-
-    constructor(raio: number, x: number, y: number) {
+var Circulo = /** @class */ (function () {
+    function Circulo(raio, x, y) {
         this.raio = raio;
         this.x = x;
         this.y = y;
     }
-    
-    getRaio() : number {
+    Circulo.prototype.getRaio = function () {
         return this.raio;
-    }
-    setRaio(r: number) : void {
+    };
+    Circulo.prototype.setRaio = function (r) {
         if (r <= 0)
             console.log('Você já estudou Geometria??');
         else if (r > 15)
             console.log('Raio não alterado. Limite ultrapassado.');
         else
             this.raio = r;
-    }
-
-    mover(x: number, y: number) : void {
+    };
+    Circulo.prototype.mover = function (x, y) {
         this.x += x; // this.x = this.x + x;
         this.y += y; // this.y = this.y + y;
-    }
-
-    aumentar(r: number) : void {
+    };
+    Circulo.prototype.aumentar = function (r) {
         this.raio += r;
-    }
-
-    exibir() : string {
-        return `Raio: ${this.raio}, X: ${this.x}, Y: ${this.y}`;
-    }
-}
-
+    };
+    Circulo.prototype.exibir = function () {
+        return "Raio: ".concat(this.raio, ", X: ").concat(this.x, ", Y: ").concat(this.y);
+    };
+    return Circulo;
+}());
 /*let circulo1 = new Circulo(2, 0, 0);
 console.log(circulo1.exibir());
 circulo1.aumentar(5);
 console.log(circulo1.exibir());
 circulo1.mover(2, 4);
 console.log(circulo1.exibir());*/
-
-let circulo2 = new Circulo(3, 5, 2);
+var circulo2 = new Circulo(3, 5, 2);
 console.log(circulo2.exibir());
 circulo2.setRaio(-4);
 console.log(circulo2.getRaio());
