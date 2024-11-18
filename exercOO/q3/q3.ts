4189f87ae23de9b81e6266bfc40425aec50780e0
Givanaldo <givanaldo@gmail.com>
2024-11-18 07:34:29 -0300

    oo

---
+++ exercOO/q3/q3.ts
@@ -0,0 +1,25 @@
+class Livro {
+    private titulo : string;
+    private autor : string;
+    private preco : number;
+
+    constructor(titulo : string, autor : string, preco : number) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+
+    alterarPreco(novo_preco : number) : void {
+        if (novo_preco >= 0)
+            this.preco = novo_preco;
+        else 
+            console.log("Não permitido valor negativo!");
+    }
+
+    descricao() : string {
+        return `Livro: ${this.titulo}\nAutor: ${this.autor}\nPreço: R$ ${this.preco.toFixed(2)}`;    }
+}
+
+let livro1 = new Livro("1984", "George Orwell", 16.09);
+let livro2 = new Livro("O Senhor dos Anéis (box)", "Tolkien", 129.90);
+let livro3 = new Livro("Admirável mundo novo", "Huxley", 34.70);
